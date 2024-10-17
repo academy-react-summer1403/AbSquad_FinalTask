4af5f5886bdfa68132089a63fdc78060de8d0ca3
Aiden <amir138204@yahoo.com>
2024-10-18 00:58:52 +0330

    let it be

--- src/Screens/Landing/LandingPage.jsx
+++ src/Screens/Landing/LandingPage.jsx
@@ -4,14 +4,9 @@ import { motion } from "framer-motion";
 const LandingPage = () => {
   return (
     <>
-      <motion.div
-        initial={{ x: "100%" }}
-        animate={{ x: 0 }}
-        exit={{ x: "-100%" }}
-        transition={{ duration: 0.1 }}
-      >
+      <div>
         <LandingApp />
-      </motion.div>
+      </div>
     </>
   );
 };
