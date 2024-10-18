c2a61dd88250676c1a730919dec48af39c7a5622
Aiden <amir138204@yahoo.com>
2024-10-18 16:40:36 +0330

    Made Course and News Conflicts

--- src/components/CourseDetailApp/LeftSide/CommentSection/index.jsx
+++ src/components/CourseDetailApp/LeftSide/CommentSection/index.jsx
@@ -3,25 +3,36 @@ import Button from "../../../Common/Button/Button";
 import { BsChatLeftText } from "react-icons/bs";
 import CommentBox from "../../../Common/CommentBox";
 import { GetNewsComments } from "../../../../core/Services/Api/DetailComments/detail.comments.api";
+import { GetCourseComments } from "../../../../core/Services/Api/CourseComments/course.comments.api";
 
-const CommentSection = ({ setCommentModalOpen, newsDetail }) => {
+const CommentSection = ({
+  setCommentModalOpen,
+  courseDetail = "",
+  newsDetail = "",
+}) => {
   const [comments, setComments] = useState([]);
-  const handleComments = async (id) => {
-    const res = await GetNewsComments(id);
-    setComments(res);
+  const handleComments = async (id, type) => {
+    if (type == "news") {
+      const res = await GetNewsComments(id);
+      setComments(res);
+    }
+    if (type == "course") {
+      const res = await GetCourseComments(id);
+      setComments(res);
+    }
   };
   useEffect(() => {
     if (newsDetail) {
-      handleComments(newsDetail.id);
+      handleComments(newsDetail.id, "news");
     }
   }, [newsDetail]);
 
   //fetching All Comments
-  // useEffect(() => {
-  //   if (courseDetail) {
-  //     handleComments(courseDetail.courseId);
-  //   }
-  // }, [courseDetail]);
+  useEffect(() => {
+    if (courseDetail) {
+      handleComments(courseDetail.courseId, "course");
+    }
+  }, [courseDetail]);
 
   return (
     <>
