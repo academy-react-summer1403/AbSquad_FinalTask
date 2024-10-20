63a8193b3b8a291a6ff412394f554520c43602f0
Aiden <amir138204@yahoo.com>
2024-10-20 16:23:34 +0330

    Made Active Header Right

---
+++ src/core/utilities/ActiveHeader/active.header.utils.js
@@ -0,0 +1,19 @@
+﻿export const handleActivePages = (setIsActive, loc) => {
+  console.log(loc);
+  let index = 0;
+  switch (loc) {
+    case "/":
+      index = 0;
+      break;
+    case "/CoursePage":
+      index = 1;
+      break;
+    case "/NewsPage":
+      index = 2;
+      break;
+    case "/ContactUs":
+      index = 3;
+      break;
+  }
+  setIsActive(index);
+};
