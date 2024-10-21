1940e3192dd45e0eeb3b532b71624eb5100cf1ad
loonydim <bbk97xx@gmail.com>
2024-10-21 20:09:40 +0330

    APIChanges

--- src/core/Services/Api/AuthPage/register/register.js
+++ src/core/Services/Api/AuthPage/register/register.js
@@ -1,7 +1,7 @@
 import http from "../../../Interceptor";
 export const PostLoginAPI = async (user = "") => {
   try {
-    const res = await http.post("/Sign/Login", user);
+    const res = await http.post("/Sign/SendVerifyMessage", user);
     return res;
   } catch (error) {}
 };
