a80b72b6ff8bae57e59217ce3f2c397be0da03e2
Aiden <amir138204@yahoo.com>
2024-10-22 19:18:58 +0330

    Made Coding Component Almost

---
+++ src/components/ContactUsApp/CodersComps/index.jsx
@@ -0,0 +1,23 @@
+﻿import { motion } from "framer-motion";
+import React from "react";
+import Aiden from "../../../assets/CoderPics/Aiden.avif";
+import WatchDogsLogo from "../../../assets/CoderPics/WatchDOgsLogo.png";
+const CoderComps = () => {
+  return (
+    // The Circle
+    <motion.div
+      className={`border-4 border-dashed border-primaryBlue rounded-full w-96 h-96 relative mx-auto flex flex-col justify-start items-center gap-3 pt-12 dark:text-primaryWhite`}
+    >
+      {/* Profile Pic */}
+      <motion.div className="w-32 h-32 rounded-full border overflow-hidden ">
+        <img src={Aiden} className="w-full h-full" />
+      </motion.div>
+
+      <div className="text-3xl">Aiden Pearce</div>
+      <span className="text-2xl">0912999444</span>
+      <span className="text-2xl">Telegram Id</span>
+    </motion.div>
+  );
+};
+
+export default CoderComps;
--- src/components/ContactUsApp/index.jsx
+++ src/components/ContactUsApp/index.jsx
@@ -1,7 +1,12 @@
 ﻿import React from "react";
+import CoderComps from "./CodersComps";
 
 const ContactUsApp = () => {
-  return <div>ContactUsApp</div>;
+  return (
+    <div className="h-96">
+      <CoderComps />
+    </div>
+  );
 };
 
 export default ContactUsApp;
