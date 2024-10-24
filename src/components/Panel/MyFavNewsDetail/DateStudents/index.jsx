58a7589b1be41067b2cfddd52d314c6383a83b27
Aiden <amir138204@yahoo.com>
2024-10-24 14:48:57 +0330

    Made The Details

---
+++ src/components/Panel/MyFavNewsDetail/DateStudents/index.jsx
@@ -0,0 +1,25 @@
+﻿import React from "react";
+import LittleRect from "../../../Common/MainCourseLittleRect";
+import { CgCalendarDates } from "react-icons/cg";
+import { IoEyeOutline } from "react-icons/io5";
+const DateStudents = () => {
+  return (
+    <div className="flex flex-col w-full gap-3 ">
+      <LittleRect
+        title={"20 اردیبهشت 1403"}
+        subText="(شروع)"
+        Icon={CgCalendarDates}
+        iconSize="25px"
+        textSize="sm:!text-xl "
+      />
+      <LittleRect
+        title={"98"}
+        Icon={IoEyeOutline}
+        iconSize="25px"
+        textSize="sm:!text-xl "
+      />
+    </div>
+  );
+};
+
+export { DateStudents };
