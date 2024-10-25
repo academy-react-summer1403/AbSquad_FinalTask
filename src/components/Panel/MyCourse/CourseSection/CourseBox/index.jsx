22a0f2d5ed59a2abb637e982c48c00455b8ab25f
Aiden <amir138204@yahoo.com>
2024-10-25 17:10:58 +0330

    MyCourse Responsive Done

--- src/components/Panel/MyCourse/CourseSection/CourseBox/index.jsx
+++ src/components/Panel/MyCourse/CourseSection/CourseBox/index.jsx
@@ -21,22 +21,22 @@ const CourseBox = ({ func }) => {
         initial={{ scale: 1 }}
         whileHover={{ scale: 1.01 }}
         transition={{ duration: 0.1, ease: "linear" }}
-        className="w-full flex flex-row justify-start items-start flex-nowrap border-b py-3 monitor:border-none rounded-2xl hover:bg-primaryGray "
+        className="w-full flex flex-row justify-start items-start flex-nowrap border-b py-3 2xl:border-none rounded-2xl hover:bg-primaryGray "
       >
         {/* Whole Rect */}
-        <div className="basis-full flex flex-row gap-y-1 monitor:flex-row monitor:items-center">
+        <div className="basis-full flex flex-row gap-y-1 2xl:flex-row 2xl:items-center">
           {/* Course Image */}
-          <div className="monitor:basis-[19.66666%] h-full basis-1/2 monitor:pr-5">
+          <div className="2xl:basis-[19.66666%] h-full basis-1/2 2xl:pr-5">
             <img
-              className=" w-11/12 monitor:w-4/5 monitor:h-[80px] h-20 rounded-lg border"
+              className=" w-11/12 2xl:w-4/5 2xl:h-[80px] h-20 rounded-lg border"
               src="/ErrImg.jpg"
               alt=""
             />
           </div>
           {/* Second part of the rect which is the rest */}
-          <div className="grow monitor:basis-full flex flex-col gap-y-1 monitor:flex-row monitor:items-center">
+          <div className="grow 2xl:basis-full flex flex-col gap-y-1 2xl:flex-row 2xl:items-center">
             {/* Name Of The Course */}
-            <div className="text-base monitor:text-xl monitor:basis-[19.33338%]">
+            <div className="text-base 2xl:text-xl 2xl:basis-[19.33338%]">
               ری اکت Js
             </div>
 
@@ -46,19 +46,19 @@ const CourseBox = ({ func }) => {
               readOnly
               disabled
               value={"محسن اسفندیاری، مهدی اصغری"}
-              className="text-fontGray text-sm monitor:text-base monitor:text-primaryBlack resize-none bg-transparent truncate h-7 monitor:basis-[21.33338%]"
+              className="text-fontGray text-sm 2xl:text-base 2xl:text-primaryBlack resize-none bg-transparent truncate h-7 2xl:basis-[21.33338%]"
             />
 
             {/* Date Of Course */}
-            <div className="text-sm text-fontGray monitor:text-primaryBlack monitor:text-base monitor:basis-[20.33338%]">
+            <div className="text-sm text-fontGray 2xl:text-primaryBlack 2xl:text-base 2xl:basis-[20.33338%] text-center">
               25 اردبیهشت 1403
             </div>
             {/* Price of Course */}
-            <div className="hidden monitor:flex monitor:text-base monitor:basis-[19.66666%]">
-              <span className="text-xl">1,800,000</span> تومان
+            <div className="hidden 2xl:flex 2xl:text-base 2xl:basis-[19.66666%]">
+              <span className="text-xl text-center">1,800,000</span> تومان
             </div>
             {/* Pay Progress Circle */}
-            <div className="hidden monitor:flex justify-start  items-center monitor:text-base monitor:grow">
+            <div className="hidden 2xl:flex justify-start  items-center 2xl:text-base 2xl:grow">
               <div className="w-16 h-16">
                 <CircularProgressbar
                   value={percentage}
@@ -72,7 +72,7 @@ const CourseBox = ({ func }) => {
                 />
               </div>
               {/*  */}
-              <span className="flex justify-center items-center mr-2">
+              <span className="flex justify-center items-center mr-2 text-nowrap">
                 پرداخت شده
               </span>
               {/* Eye Button */}
--- src/components/Panel/MyCourse/PageTitle.jsx
+++ src/components/Panel/MyCourse/PageTitle.jsx
@@ -4,10 +4,10 @@ import Button from "../../Common/Button/Button";
 const PageTitle = ({ handleOpenFilter }) => {
   return (
     <div className="flex justify-between w-full mb-7">
-      <h1 className="monitor:text-4xl text-2xl">دوره من</h1>
+      <h1 className="2xl:text-4xl text-2xl">دوره من</h1>
       <Button
         text={"فیلتر"}
-        phoneStyle={"monitor:hidden h-10"}
+        phoneStyle={"2xl:hidden h-10"}
         onClick={handleOpenFilter}
       />
     </div>
