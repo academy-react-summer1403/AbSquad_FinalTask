5dba3849fc599e06bc0d23b0af8564aa2038f1ae
Aiden <amir138204@yahoo.com>
2024-10-27 23:20:24 +0330

    Fixed Buttons For activing Button

--- src/components/Common/LikeDislikeCircle/index.jsx
+++ src/components/Common/LikeDislikeCircle/index.jsx
@@ -5,12 +5,12 @@ import { GetAllFavArticle } from "../../../core/Services/Api/Student/AddDeleteFa
 import { DeleteFavArticle } from "../../../core/Services/Api/Student/AddDeleteFavNews/delete.fav.api";
 const LikeDislikeCircle = ({ Icon, iconSize, style = "", newsId }) => {
   const [favArticles, setFavArticles] = useState([]);
+  const [isFavBtn, setIsFavBtn] = useState("deactive");
 
   const handleAddFav = async (id) => {
     const foundArticle = favArticles.find((it) => {
       return it.newsId == id;
     });
-
     if (foundArticle == undefined) {
       await AddFavArticle(id);
     } else if (foundArticle != undefined) {
@@ -28,13 +28,32 @@ const LikeDislikeCircle = ({ Icon, iconSize, style = "", newsId }) => {
     handleGetAllFavArticle();
   }, []);
 
+  // Button Activing
+  const handleIsFavBtn = (id) => {
+    const foundArticle = favArticles.find((it) => {
+      return it.newsId == id;
+    });
+    if (foundArticle == undefined) {
+      setIsFavBtn("deactive");
+    } else {
+      setIsFavBtn("active");
+    }
+  };
+  useEffect(() => {
+    if (favArticles) {
+      handleIsFavBtn(newsId);
+    }
+  }, [favArticles]);
+
   return (
     <>
       <div
         onClick={() => {
           handleAddFav(newsId);
         }}
-        className={`p-4 rounded-full border border-primaryGray flex justify-center items-center ${style} cursor-pointer`}
+        className={`p-4 rounded-full border border-primaryGray flex justify-center items-center ${style} cursor-pointer ${
+          isFavBtn == "active" ? "!bg-primaryBlue border-none" : ""
+        }`}
       >
         <Icon size={`${iconSize}`} className={`dark:text-primaryWhite`} />
       </div>
