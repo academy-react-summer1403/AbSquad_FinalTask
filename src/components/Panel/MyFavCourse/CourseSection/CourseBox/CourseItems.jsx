f97c58116511be299a1c0e521c78a760d7ec5be7
Aiden <amir138204@yahoo.com>
2024-10-28 16:34:14 +0330

    finished APi before deletion from courses

--- src/components/Panel/MyFavCourse/CourseSection/CourseBox/CourseItems.jsx
+++ src/components/Panel/MyFavCourse/CourseSection/CourseBox/CourseItems.jsx
@@ -1,10 +1,36 @@
-﻿import React from "react";
+﻿import React, { useEffect, useState } from "react";
 import { IoEyeOutline } from "react-icons/io5";
 import { motion } from "framer-motion";
 import { LuBookDown } from "react-icons/lu";
 import { RxCross1 } from "react-icons/rx";
 import "react-circular-progressbar/dist/styles.css";
-const CourseItems = ({ func }) => {
+import { GetCourseDetail } from "../../../../../core/Services/Api/CourseDetail/course.detail.api";
+import { handleDateFormat } from "../../../../../core/utilities/DateConverter/data.convert.utils";
+
+const CourseItems = ({
+  func,
+  courseId,
+  title,
+  image,
+  favId,
+  handleGetAllFavCourse,
+}) => {
+  const [detailCourse, setDetailCourse] = useState({});
+  const handleDetailCourse = async (id) => {
+    const res = await GetCourseDetail("CourseId=" + id);
+    setDetailCourse(res);
+  };
+  useEffect(() => {
+    if (courseId) handleDetailCourse(courseId);
+  }, [courseId]);
+
+  // Converting Dates ********************************************************************
+  const [formattedDateStart, setFormattedDateStart] = useState("");
+  useEffect(() => {
+    if (detailCourse.startTime !== undefined)
+      setFormattedDateStart(handleDateFormat(detailCourse.startTime));
+  }, [detailCourse.startTime]);
+  // Render Start ***********************************************************************
   return (
     <>
       <motion.div
@@ -19,33 +45,34 @@ const CourseItems = ({ func }) => {
           <div className="2xl:basis-[17.66666%] h-full basis-1/2 2xl:pr-5">
             <img
               className=" w-11/12 2xl:w-4/5 2xl:h-[80px] h-20 rounded-lg border"
-              src="/ErrImg.jpg"
+              src={image ? image : "/ErrImg.jpg"}
               alt=""
             />
           </div>
           {/* Second Part Of the Div */}
           <div className="grow 2xl:basis-full flex flex-col gap-y-1 2xl:flex-row 2xl:items-center">
             {/* Name Of The Course */}
-            <div className="text-base 2xl:text-xl 2xl:basis-[18%]">
-              ری اکت Js
-            </div>
+            <div className="text-base 2xl:text-xl 2xl:basis-[18%]">{title}</div>
             {/* Teachers */}
             <div className="2xl:basis-[18%]">
               <textarea
                 maxLength={10}
                 readOnly
                 disabled
-                value={"محسن اسفندیاری، مهدی اصغری"}
+                value={detailCourse.teacherName}
                 className="text-fontGray text-sm 2xl:text-base 2xl:text-primaryBlack resize-none bg-transparent truncate h-7 2xl:basis-[21.33338%] 2xl:w-[70%]"
               />
             </div>
             {/* Date Of Course */}
             <div className="text-sm text-fontGray 2xl:text-primaryBlack 2xl:text-base 2xl:basis-[17.5%]">
-              25 اردبیهشت 1403
+              {formattedDateStart}
             </div>
             {/* Price of Course */}
             <div className="hidden 2xl:flex 2xl:text-base 2xl:basis-[16.66666%]">
-              <span className="text-xl">1,800,000</span> تومان
+              <span className="text-xl">
+                {parseInt(detailCourse.cost).toLocaleString()}
+              </span>{" "}
+              تومان
             </div>
             {/* Icons */}
             <div className="hidden 2xl:flex justify-end  items-center 2xl:text-base 2xl:grow">
--- src/components/Panel/MyFavCourse/CourseSection/CourseBox/index.jsx
+++ src/components/Panel/MyFavCourse/CourseSection/CourseBox/index.jsx
@@ -1,18 +1,33 @@
-﻿import React from "react";
+﻿import React, { useEffect, useState } from "react";
 import { CourseItems } from "./CourseItems";
+import { GetAllFavCourse } from "../../../../../core/Services/Api/Student/AddDeleteFavCourse/GetAllCourse.api";
 const CourseBox = ({ func }) => {
+  // Getting All Favorited News
+  const [favArticles, setFavArticles] = useState([]);
+  const handleGetAllFavCourse = async () => {
+    const res = await GetAllFavCourse();
+    setFavArticles(res.favoriteCourseDto);
+  };
+  useEffect(() => {
+    handleGetAllFavCourse();
+  }, []);
+
   return (
     <>
       {/* Items   */}
-      <CourseItems func={func} />
-      <CourseItems func={func} />
-      <CourseItems func={func} />
-      <CourseItems func={func} />
-      <CourseItems func={func} />
-      <CourseItems func={func} />
-      <CourseItems func={func} />
-      <CourseItems func={func} />
-      <CourseItems func={func} />
+      {favArticles.map((it, index) => {
+        return (
+          <CourseItems
+            key={index}
+            func={func}
+            courseId={it.courseId}
+            title={it.courseTitle}
+            image={it.tumbImageAddress}
+            favId={it.favoriteId}
+            handleGetAllFavCourse={handleGetAllFavCourse}
+          />
+        );
+      })}
     </>
   );
 };
