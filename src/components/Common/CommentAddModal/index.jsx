b90ab8b1b9365b8f14ef8d369d1d6e78e2963bd5
Aiden <amir138204@yahoo.com>
2024-12-07 03:54:24 +0330

    Fixed comment modal position

--- src/components/Common/CommentAddModal/index.jsx
+++ src/components/Common/CommentAddModal/index.jsx
@@ -6,9 +6,9 @@ const CommentAddModal = ({ onClickFunc, modalTitle }) => {
   return (
     <>
       {/* White Div */}
-      <div className=" absolute top-0 left-0 w-screen h-full z-40 bg-black bg-opacity-50">
+      <div className=" absolute top-0 left-0 w-screen h-full z-40 bg-black bg-opacity-50 overflow-y-scroll">
         {/* Add Comment Box  */}
-        <div className="flex flex-col items-center justify-start w-[1020px] h-[805px] right-[33%] top-[10%] fixed z-50 bg-primaryWhite border dark:bg-primaryBlack dark:text-primaryWhite border-primaryGray rounded-3xl p-6 max-sm:right-0 max-sm:w-full max-sm:h-max max-sm:absolute max-sm:top-0 max-sm:rounded-none">
+        <div className="flex flex-col items-center justify-start w-[70%] h-full right-[15%] top-[0%] fixed z-50 bg-primaryWhite border dark:bg-primaryBlack dark:text-primaryWhite border-primaryGray rounded-3xl p-6 max-sm:right-0 max-sm:w-full max-sm:h-max max-sm:absolute max-sm:top-0 max-sm:rounded-none">
           {/* Title And Close Btn */}
           <ModalTitles modalTitle={modalTitle} onClickFunc={onClickFunc} />
           {/* Comments */}
